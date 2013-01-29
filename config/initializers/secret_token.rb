e9a50233a53db30840968bde332f43b490809153
Kaid <kaid@kaid.me>
2013-01-29 10:41:06 +0800

    edushare mobile demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EdushareMobileDemo::Application.config.secret_token = '26c483a2266d2f90165f8cd7d20f66f7e261f2a0b26fe962213d1c6c2a0945c73ec48f0e55c32c9f1e047aa5c6109dd0aab226bd794e82ec0a86ab197eba772c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+EdushareMobileDemo::Application.config.session_store :cookie_store, :key => '_edushare-mobile-demo_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# EdushareMobileDemo::Application.config.session_store :active_record_store
