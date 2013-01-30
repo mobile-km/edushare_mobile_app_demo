3ce63181f5a0df135ba91ed91978855ab15dc752
mykingla <kingla_pei@163.com>
2013-01-30 15:32:48 +0800

    Add data of a1, b1, b2 data

---
+++ app/models/book.rb
@@ -0,0 +1,32 @@
+# coding: utf-8
+class Book < DemoModel
+  attr_accessor :title, :cover_img
+
+  def self.make_all
+    a1_1 = self.new
+    a1_1.title = 'Ruby 重构'
+    a1_1.cover_img = ''
+
+    a1_2 = self.new
+    a1_2.title = 'Jquery 在线 API'
+    a1_2.cover_img = ''
+
+    a1_3 = self.new
+    a1_3.title = 'Java 重构'
+    a1_3.cover_img = ''
+
+
+    a1_4 = self.new
+    a1_4.title = 'Thinking in Java'
+    a1_4.cover_img = ''
+
+
+    a1_5 = self.new
+    a1_5.title = 'Head first Java'
+    a1_5.cover_img = ''
+
+
+    [a1_1, a1_2, a1_3, a1_4, a1_5]
+  end
+  
+end
