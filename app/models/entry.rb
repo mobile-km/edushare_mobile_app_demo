77f98a82be1a3efc8e48ef7a77f632b917b88182
Kaid <kaid@kaid.me>
2013-01-31 18:50:57 +0800

    modified section and entry

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,11 +3,9 @@ class Entry < DemoModel
   attr_accessor :section, :title, :content
 
   def self.make_all
-    @jquery7_8 = self.load_jquery_entries('7-8')
-    [@jquery7_8]
+    Section.all.map do |section|
+      section.entries
+    end.flatten
   end
 
-  def self.load_jquery_entries(sections)
-    YAML.load File.open("lib/data/entries/jquery#{sections}.yaml")
-  end
 end
