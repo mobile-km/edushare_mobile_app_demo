80f3e3225457b1e018aeeb5c05f1a5235d34c9df
mykingla <kingla_pei@163.com>
2013-02-01 15:11:43 +0800

    Fix d1

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -4,22 +4,23 @@ class Bookmark < DemoModel
 
   def self.list_all
     entries = Book.find(1).sections[1].entries
-    entry_1 = entries[rand(entries.length)]
+    entry_1 = entries[1]
 
     entries = Book.find(1).sections[2].entries
-    entry_2 = entries[rand(entries.length)]
+    entry_2 = entries[2]
 
     entries = Book.find(1).sections[3].entries
-    entry_3 = entries[rand(entries.length)]
+    entry_3 = entries[3]
 
     entries = Book.find(1).sections[4].entries
-    entry_4 = entries[rand(entries.length)]
+    entry_4 = entries[4]
 
     entries = Book.find(1).sections[5].entries
-    entry_5 = entries[rand(entries.length)]
+    entry_5 = entries[5]
 
     [entry_1, entry_2, entry_3, entry_4, entry_5]
   end
+
 
 
   def self.make_all
