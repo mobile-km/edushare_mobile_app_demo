744f4c4c3ea43128b985c3a789cec2ee57303f62
ben7th <ben7th@sina.com>
2013-02-01 17:38:03 +0800

    调整课本页面

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
+  def page_top_bar(title, back_to_href)
+    render 'index/parts/page_top_bar', :title => title, 
+                                       :href => back_to_href
+  end
+
   def page_top_nav(current)
     render 'index/parts/page_top_nav', :current => current
+  end
+
+  def page_bottom_nav
+    render 'index/parts/page_bottom_nav'
   end
 end
