f39b6045473a53b3bc525c15b0b1c81fe4cdb5c3
fushang318 <fushang318@gmail.com>
2013-02-21 18:51:25 +0800

    fix

--- lib/text_cell_parser.rb
+++ lib/text_cell_parser.rb
@@ -2,8 +2,8 @@ module TextCellParser
   CONFIG = YAML.load_file(Rails.root.join("config/text_cell_config.yaml"))
   DATA = YAML.load_file(Rails.root.join("lib/text_cell_data/nav_items.yaml"))
 
-  def self.root_navs
-    TextCellParser::Nav.root_navs
+  def self.roots
+    TextCellParser::TextCell.roots
   end
 
 end
