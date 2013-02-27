d80a7f92e482d0903e8346311131ac17998548a0
mykingla <kingla_pei@163.com>
2013-02-27 15:27:53 +0800

    fix

--- app/controllers/admin/p_text_cell_controller.rb
+++ app/controllers/admin/p_text_cell_controller.rb
@@ -1,13 +1,23 @@
-class Admin::PTexCellController < ApplicationController
-  before_filter :per_load
+class Admin::PTextCellController < ApplicationController
+  before_filter :pre_load
 
-  def per_load
+  def pre_load
     @text_cell = PTextCell.find params[:id] if params[:id]
   end
 
 
   def index
-    @text_cells = PTextCell.all
+    @p_text_cells = PTextCell.all
+  end
+
+  def new
+    @text_cell = PTextCell.new
+  end
+
+
+  def create
+    PTextCell.create(params[:text_cell])
+    redirect_to "/admin/p_text_cell"
   end
 
   def show
@@ -21,12 +31,12 @@ class Admin::PTexCellController < ApplicationController
     @text_cell.update_attributes(params[:p_text_cell])
 
     if @text_cell.update_attributes(params[:p_text_cell])
-      return redirect_to "/admin/text_cell"
+      return redirect_to "/admin/p_text_cell"
     end
 
     error = @text_cell.errors.first
     flash[:error] = error[1]
-    redirect_to "/admin/text_cell/#{@text_cell.id}/edit"
+    redirect_to "/admin/p_text_cell/#{@text_cell.id}/edit"
   end
 
   def destroy
