aa135db990883c110392c867bee5b9897af1f8d5
mykingla <kingla_pei@163.com>
2013-02-27 14:54:07 +0800

    Crud of text cell

--- app/controllers/admin/p_text_cell_controller.rb
+++ app/controllers/admin/p_text_cell_controller.rb
@@ -1,5 +1,13 @@
 class Admin::PTexCellController < ApplicationController
+  before_filter :per_load
+
+  def per_load
+    @text_cell = PTextCell.find params[:id] if params[:id]
+  end
+
+
   def index
+    @text_cells = PTextCell.all
   end
 
   def show
@@ -10,8 +18,18 @@ class Admin::PTexCellController < ApplicationController
   end
 
   def update
+    @text_cell.update_attributes(params[:p_text_cell])
+
+    if @text_cell.update_attributes(params[:p_text_cell])
+      return redirect_to "/admin/text_cell"
+    end
+
+    error = @text_cell.errors.first
+    flash[:error] = error[1]
+    redirect_to "/admin/text_cell/#{@text_cell.id}/edit"
   end
 
   def destroy
+    @text_cell.destroy
   end
 end
