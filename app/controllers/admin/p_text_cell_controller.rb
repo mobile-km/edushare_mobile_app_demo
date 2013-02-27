1653bceb52db13e5b130f1ad6a410c4d48ebd978
menxu <menxu_work@163.com>
2013-02-27 14:46:10 +0800

    提交

--- app/controllers/admin/p_text_cell_controller.rb
+++ app/controllers/admin/p_text_cell_controller.rb
@@ -1,3 +1,33 @@
-class Admin::PTexCellController < ApplicationController
+class Admin::PTextCellController < ApplicationController
+
+  layout 'web_work'
   
+  def index
+    
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+  
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end
