3f7c2ddbc4fe5cf97ee97ff9216dda15bf4964ad
mykingla <kingla_pei@163.com>
2013-02-27 15:32:11 +0800

    change controller name

---
+++ app/controllers/admin/p_text_cells_controller.rb
@@ -0,0 +1,45 @@
+class Admin::PTextCellsController < ApplicationController
+  before_filter :pre_load
+
+  def pre_load
+    @p_text_cell = PTextCell.find params[:id] if params[:id]
+  end
+
+
+  def index
+    @p_text_cells = PTextCell.all
+  end
+
+  def new
+    @p_text_cell = PTextCell.new
+  end
+
+
+  def create
+    PTextCell.create(params[:p_text_cell])
+    redirect_to "/admin/p_text_cell"
+  end
+
+  def show
+  end
+
+
+  def edit
+  end
+
+  def update
+    @p_text_cell.update_attributes(params[:p_text_cell])
+
+    if @text_cell.update_attributes(params[:p_text_cell])
+      return redirect_to "/admin/p_text_cell"
+    end
+
+    error = @text_cell.errors.first
+    flash[:error] = error[1]
+    redirect_to "/admin/p_text_cell/#{@p_text_cell.id}/edit"
+  end
+
+  def destroy
+    @p_text_cell.destroy
+  end
+end
