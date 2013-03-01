06b7433a7078309052a126af70914e0a7d80dbd6
fushang318 <fushang318@gmail.com>
2013-03-01 17:21:14 +0800

    import rb

--- script/custom/import_text_cell_from_yaml.rb
+++ script/custom/import_text_cell_from_yaml.rb
@@ -1,4 +1,6 @@
 PTextCell.destroy_all
+Attr.destroy_all
+Image.destroy_all
 
 yaml_file_path = ARGV[0]
 if !yaml_file_path.blank?
@@ -11,22 +13,45 @@ else
   roots = TextCellYamlParser::TextCell.roots
 end
 
-# roots(yaml_file_path = "lib/text_cell_data/nav_items.yaml")
-
 def yaml_to_text_cell(yaml_text_cell, parent_text_cell = nil)
-  hash = yaml_text_cell.hash
+  text_cell_hash = yaml_text_cell.hash
 
   tc = PTextCell.new
-  tc.title = hash["title"]
-  tc.desc = hash["desc"]
-  # tc.images = [hash["images"]].flatten
-  # tc.rcover = hash["cover"]
-  tc.rformat = hash["format"]
+  tc.title = text_cell_hash["title"]
+  tc.desc = text_cell_hash["desc"]
+
+  if !text_cell_hash["cover"].blank?
+    cover_path = Rails.root.join(text_cell_hash["cover"])
+    if !File.exists?(cover_path)
+      raise "#{cover_path} 文件不存在"
+    end
+    tc.cover = File.open(Rails.root.join(text_cell_hash["cover"]))
+  end
+
+  
+  tc.rformat = text_cell_hash["format"]
   if !parent_text_cell.blank?
     tc.parent = parent_text_cell
   end
+
   tc.save
-  tc.update_attrs(hash["attrs"]) if !hash["attrs"].blank?
+
+  if !text_cell_hash["images"].blank?
+    [text_cell_hash["images"]].flatten.each do |hash|
+      tc.images.create(
+        :image => File.open(Rails.root.join(hash["image"])),
+        :alt => hash["alt"]
+      )
+    end
+  end
+
+  if !text_cell_hash["attrs"].blank?
+    text_cell_hash["attrs"].each do |hash|
+      arr = hash.to_a.flatten
+      tc.rattrs.create(:key => arr[0], :value => arr[1])
+    end
+  end
+
   p "#{tc.order} save success"
 
   yaml_text_cell.children.each do |child_yaml_text_cell|
